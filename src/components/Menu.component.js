ca078f6191027b7cf7e0ee1c44220340bbd6319d
unknown <rahulrd005>
2018-09-15 22:16:38 +0530

    ftr : show selected dish

--- src/components/Menu.component.js
+++ src/components/Menu.component.js
@@ -8,15 +8,39 @@ class Menu extends Component {
         super(props);
         
         this.state = {
-            dishes: props.dishes
+            dishes: props.dishes,
+            selectedDish : null
         }
+    }
+
+    renderDish(dish){
+        if ( dish !=null ) {
+            return (
+                <div className="col-md-6" >
+                    <Card  >
+                        <CardImage className="img-card" src ={dish.image} alt={dish.name} ></CardImage>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>   
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+              </div>
+            )
+        }else{
+            return '';
+        }
+    }
+
+    onDishSelected( dish ){
+        console.log(dish);
+        this.setState({selectedDish : dish});
     }
 
     render() {
         const menu = this.state.dishes.map((dish) => {
             return (
-              <div key={dish.id} className="col-md-3 col-sm-12 mt-5">
-                <Card  >
+              <div key={dish.id} className="col-md-6 col-sm-12 mt-5" onClick={() => this.onDishSelected(dish)}>
+                <Card  > 
                   <CardImage className="img-card" src ={dish.image} alt={dish.name} ></CardImage>
                   <CardBody   >
                     <CardTitle>{dish.name}</CardTitle>
@@ -31,9 +55,15 @@ class Menu extends Component {
         return (
           <div className="container">
             <div className="row">
-              
                   {menu}
-              
+            </div>
+            <div className="row mt-5">
+                <div className = "col-md-12" >
+                <h3>You selected this dish </h3>
+                </div>
+                <div className="col-md-12" >
+                    {this.renderDish( this.state.selectedDish)}
+                </div>
             </div>
           </div>
         );
